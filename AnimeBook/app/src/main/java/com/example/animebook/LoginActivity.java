f5fe8eaf9a5e89ba6ff6391232d76575a4683e5f
abidh0601 <abidh0601@gmail.com>
2021-04-23 19:11:38 -0400

    Login page setup (still needs functionality)

---
+++ AnimeBook/app/src/main/java/com/example/animebook/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.animebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "Login Activity";
+
+    EditText etUsername;
+    EditText etPassword;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //TODO : if user is already logged in go to main activity
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "btnLogin clicked");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Trying to login user:" + username);
+    }
+}
