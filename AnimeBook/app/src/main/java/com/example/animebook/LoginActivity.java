55dd77296e13c4daf74b2c8af6cc84b17bcf1651
abidh0601 <abidh0601@gmail.com>
2021-04-23 19:39:44 -0400

    Implemented logging in to app

--- AnimeBook/app/src/main/java/com/example/animebook/LoginActivity.java
+++ AnimeBook/app/src/main/java/com/example/animebook/LoginActivity.java
@@ -2,11 +2,17 @@ package com.example.animebook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "Login Activity";
@@ -20,7 +26,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //TODO : if user is already logged in go to main activity
+        //If user is already logged in go to main activity
+        if(ParseUser.getCurrentUser() != null){
+            goToMainActivity();
+        }
 
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
@@ -40,5 +49,27 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String username, String password) {
         Log.i(TAG, "Trying to login user:" + username);
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Issue with login", e);
+                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                goToMainActivity();
+                Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goToMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        //Removes login activity from backstack
+        finish();
+
     }
 }
