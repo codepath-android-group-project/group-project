81b284f0fffd7b23796751841fe5d0dde7a44a71
abidh0601 <abidh0601@gmail.com>
2021-04-23 22:09:44 -0400

    Implemented Register Activity

---
+++ AnimeBook/app/src/main/java/com/example/animebook/RegisterActivity.java
@@ -0,0 +1,101 @@
+package com.example.animebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class RegisterActivity extends AppCompatActivity {
+    public static final String TAG = "RegisterActivity";
+
+    EditText etUsername;
+    EditText etPassword;
+    EditText etConfirmPassword;
+    Button btnRegister;
+    TextView tvLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        tvLogin = findViewById(R.id.tvLogin);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String confirmPassword = etConfirmPassword.getText().toString();
+
+                if(username.isEmpty() || password.isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Username or password cannot be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if(!password.equals(confirmPassword)){
+                    Toast.makeText(RegisterActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                registerUser(username, password);
+            }
+        });
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLoginActivity();
+            }
+        });
+
+    }
+
+    private void registerUser(String username, String password) {
+
+        ParseUser user = new ParseUser();
+
+        user.setUsername(username);
+        user.setPassword(password);
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with register", e);
+                    Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+               goToMainActivity();
+            }
+        });
+
+
+    }
+
+    private void goToMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        finishAffinity();
+        startActivity(i);
+
+    }
+
+    private void goToLoginActivity(){
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+}
